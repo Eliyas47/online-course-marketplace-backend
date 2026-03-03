f7da00d6f9cd4a8c9d30fad0971998f488283d03
Eliyas47 <elansbiratu23.com>
2026-03-03 21:06:41 +0300

    Fix DATABASES duplicate default error

--- config/settings.py
+++ config/settings.py
@@ -87,10 +87,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database (Render PostgreSQL)
 # =========================
 
+import dj_database_url
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+    "default": dj_database_url.config(
+        default="sqlite:///" + str(BASE_DIR / "db.sqlite3"),
         conn_max_age=600,
     )
 }
